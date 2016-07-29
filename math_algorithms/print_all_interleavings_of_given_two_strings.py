04a59a02bb7f6b3fca961ae8566724df29a48ba1
aishs8 <s.its.aishu@gmail.com>
2016-07-28 21:33:53 -0500

    math algorithms

---
+++ math_algorithms/print_all_interleavings_of_given_two_strings.py
@@ -0,0 +1,25 @@
+"""
+http://www.geeksforgeeks.org/print-all-interleavings-of-given-two-strings/
+"""
+
+def print_all_interleavings_of_given_two_strings(result_string, string1, string2, len_string1, len_string2, res_index):
+
+	if len_string1 == 0 and len_string2 == 0:
+		print ''.join(result_string)
+
+	if len_string1!= 0:
+		result_string[res_index] = string1[0]
+		print_all_interleavings_of_given_two_strings(result_string, string1[1:], string2, len_string1-1, len_string2, res_index+1)
+
+	if len_string2!= 0:
+		result_string[res_index] = string2[0]
+		print_all_interleavings_of_given_two_strings(result_string, string1, string2[1:], len_string1, len_string2-1, res_index+1)
+
+
+if __name__ == "__main__":
+	string1 = "de"
+	string2 = "p"
+	len_string1 = len(string1)
+	len_string2 = len(string2)
+	result_string = ['']*(len_string1+len_string2)
+	print_all_interleavings_of_given_two_strings(result_string, string1, string2, len_string1, len_string2, 0)
