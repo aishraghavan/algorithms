2d3d2479101459619805615c5f1518de3e9172e1
aishs8 <s.its.aishu@gmail.com>
2016-09-03 13:59:59 -0500

    kth_non_repeating_character list

--- string_manipulation/kth_non_repeating_character.py
+++ string_manipulation/kth_non_repeating_character.py
@@ -17,16 +17,16 @@ two_loop_non_repeating_count called with ('geeksforgeeks', 3) => Result : r
 ----------------
 Invalid cases:
 ----------------
-hash_map_non_repeating_count called with ('geeksforgeeks', 0) => Result : Invalid input: Please check.
-hash_map_non_repeating_count called with ('geeksforgeeks', 45) => Result : Invalid input: Please check.
-hash_map_non_repeating_count called with (None, 45) => Result : Invalid input: Please check.
-hash_map_non_repeating_count called with ('geeksforgeeks', 5) => Result : Invalid input: Please check.
+list_non_repeating_count called with ('geeksforgeeks', 0) => Result : Invalid input: Please check.
+list_non_repeating_count called with ('geeksforgeeks', 45) => Result : Invalid input: Please check.
+list_non_repeating_count called with (None, 45) => Result : Invalid input: Please check.
+list_non_repeating_count called with ('geeksforgeeks', 5) => Result : Invalid input: Please check.
 ----------------
 Valid cases:
 ----------------
-hash_map_non_repeating_count called with ('geeksforgeeks', 1) => Result : f
-hash_map_non_repeating_count called with ('geeksforgeeks', 2) => Result : o
-hash_map_non_repeating_count called with ('geeksforgeeks', 3) => Result : r
+list_non_repeating_count called with ('geeksforgeeks', 1) => Result : f
+list_non_repeating_count called with ('geeksforgeeks', 2) => Result : o
+list_non_repeating_count called with ('geeksforgeeks', 3) => Result : r
 
 """
 def two_loop_non_repeating_count(string, k):
@@ -43,20 +43,18 @@ def two_loop_non_repeating_count(string, k):
     if non_repeating_count <k:
         return "Invalid input: Please check."
 
-def hash_map_non_repeating_count(string, k):
+def list_non_repeating_count(string, k):
     if not string or k<=0 or k>len(string):
         return "Invalid input: Please check."
-    hash_map = {}
+
     length = len(string)
     non_repeated_chars = []
     for index1 in range(length):
-        if string[index1] not in hash_map.keys():
+        if string[index1] not in non_repeated_chars:
             non_repeated_chars.append(string[index1])
-            hash_map[string[index1]] = 1
         else:
-            if string[index1] in non_repeated_chars:
-                non_repeated_chars.remove(string[index1])
-            hash_map[string[index1]] += 1
+            non_repeated_chars.remove(string[index1])
+
     if len(non_repeated_chars) <k:
         return "Invalid input: Please check."
     return non_repeated_chars[k-1]
@@ -72,7 +70,7 @@ def pretty_output_printer(function_name, *custom_args):
 
 if __name__ == "__main__":
     string = "geeksforgeeks"
-    function_names = [two_loop_non_repeating_count, hash_map_non_repeating_count]
+    function_names = [two_loop_non_repeating_count, list_non_repeating_count]
     for function_name in function_names:
         pretty_header_printer("Invalid cases:")
         pretty_output_printer(function_name, string, 0)
